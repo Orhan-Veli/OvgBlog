1a1213634418099fc87b3ccf660f5a48b1afe0c4
Orhan-Veli <orhan_1576@hotmail.com>
2021-02-14 13:48:42 +0300

    Base Entity Refactor

--- OvgBlog.DAL/Data/Entities/BaseEntity.cs
+++ OvgBlog.DAL/Data/Entities/BaseEntity.cs
@@ -7,12 +7,12 @@ namespace OvgBlog.DAL.Data.Entities
 {
     public class BaseEntity:IEntity
     {
-        public Guid Id { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime DeletedDate { get; set; }
-        public DateTime UpdatedDate { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
 
 
     }
